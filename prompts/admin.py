17c174a862534c053fb0bcf0773c8907cdbbd991
Adam <adam.b.ganong@gmail.com>
2018-02-19 20:30:09 -0400

    models completed, SQL added for a mirrored set up of prompt_genres, and prompt_piece_types

--- prompts/admin.py
+++ prompts/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Genre, PromptPiece, BuiltPrompt, PieceType
 # Register your models here.
+
+admin.site.register(Genre)
+admin.site.register(PromptPiece)
+admin.site.register(BuiltPrompt)
+admin.site.register(PieceType)
