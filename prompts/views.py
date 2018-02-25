11007375051242cc9c2808b3454f0da89f75bcff
Adam <adam.b.ganong@gmail.com>
2018-02-24 20:38:59 -0400

    testing

--- prompts/views.py
+++ prompts/views.py
@@ -1,2 +1,9 @@
-from django
+from django.shortcuts import get_object_or_404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Genre, PromptPiece, BuiltPrompt, PieceType
+from .serializer import GenreSerializer
 # Create your views here.
+
+
