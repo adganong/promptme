5a2138ebfdeb4f8602c528f9c852101209aea5aa
Adam <adam.b.ganong@gmail.com>
2018-02-24 22:32:44 -0400

    trying to fix error

--- prompts/views.py
+++ prompts/views.py
@@ -1,18 +1,2 @@
-from django.shortcuts import get_object_or_404
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
-from .models import Genre, PromptPiece, BuiltPrompt, PieceType
-from .serializer import GenreSerializer
+from django
 # Create your views here.
-
-
-class GenreList(APIView):
-
-    def get(self, request):
-        genre = Genre.objects.all()
-        serializer = GenreSerializer(genre, many=True)
-        return Response(serializer.data)
-
-    # There is no post for this, that is a strategic choice.
-    # Any additions to the Genre table MUST be done through the admin view.
